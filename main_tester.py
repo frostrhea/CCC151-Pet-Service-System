402e5f6d1574fb22729b58c9c7727f973dc73f2b
Airplanejuju <88552693+Airplanejuju@users.noreply.github.com>
2023-07-04 05:32:10 +0800

    some changes on tuples(?) and returnID function on class Service

--- main_tester.py
+++ main_tester.py
@@ -12,7 +12,8 @@ def main():
     #service.addService('Trim','50')
     #service.addService('Ear Cleaning','80')
     #service.addService('Fur Cleaning','100')
-    appointment.addAppointment('2023-07-04','12:05','Reservation','Pending','Rango','Dog', 'Aspin', 'Gel', '09978101451', ['Fur Cleaning', 'Trim', 'Ear Cleaning'])
+    services = [('Fur Cleaning'), ('Trim'), ('Ear Cleaning')]
+    appointment.addAppointment('2023-07-04','12:05','Reservation','Pending','Rango','Dog', 'Aspin', 'Gel', '09978101451', services)
  
 
 main()
