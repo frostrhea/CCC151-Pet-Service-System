0559acfc34e8b848521dc71234c1d18557b421b9
Airplanejuju <88552693+Airplanejuju@users.noreply.github.com>
2023-07-04 00:35:57 +0800

    some changes

--- main_tester.py
+++ main_tester.py
@@ -1,9 +1,17 @@
-from appointment_copy import Service
+from appointment import Appointment, Service, Owner, Pet, Appointment_Service
+
 
 def main():
 
     service = Service()
+    owner = Owner()
+    pet = Pet() 
+    appointment = Appointment()
+    appointment_service = Appointment_Service()
+
+    #service.addService('','Trim','100')
+    appointment.addAppointment('','2023-07-04','12:05','Reservation','Pending','','')
     
-    service.addService('','Trim','100')
+
     
 main()
