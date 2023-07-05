6e7edb2ccbe14254292b8b851e115f9079dff1de
Airplanejuju <88552693+Airplanejuju@users.noreply.github.com>
2023-07-06 01:19:49 +0800

    ...

--- main-tester.py
+++ main-tester.py
@@ -12,9 +12,9 @@ def main():
     #service.addService('Trim','50')
     #service.addService('Ear Cleaning','80')
     #service.addService('Fur Cleaning','100')
-    #appointment.addAppointment('2023-07-05','13:05','Reservation','Pending','Jake','Dog', 'Aspin', 'Gel', '09978101451', [('Fur Cleaning', 'Trim')])
+    #appointment.addAppointment('2023-07-05','13:05','Reservation','Pending','Jake','Dog', 'Aspin', 'Gel', '09978101451', [('Fur Cleaning',)])
     #appointment.addAppointment('2023-07-04','12:05','Reservation','Pending','Rango','Dog', 'Aspin', 'Gel', '09978101451', [('Fur Cleaning', 'Trim', 'Ear Cleaning')])
-    #appointment.deleteAppointment([7141])
+    #appointment.deleteAppointment([6271])
     #service.deleteService([1537])
     
 main()
